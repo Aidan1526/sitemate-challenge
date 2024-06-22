d6b05a93112f2654ba789c6178cef2691ca8011c
Aidan Bui <aidan@techreative.com.au>
2024-06-22 10:25:54 +1000

    Setup models and serializers, refactored items to issues

--- server/app/models.py
+++ server/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    item_id = models.CharField(max_length=10)
+class Issue(models.Model):
+    issue_id = models.CharField(max_length=10)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=99)
--- server/app/views.py
+++ server/app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Issue
+from .serializers import IssueSerializer
 
-# Create your views here.
+class IssueApiView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        issues = Issue.objects.filter(user = request.user.id)
+        serializer = IssueSerializer(issues, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'issue_id': request.data.get('issue_id'), 
+            'title': request.data.get('title'), 
+            'description': request.data.get('description'), 
+        }
+        serializer = IssueSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
