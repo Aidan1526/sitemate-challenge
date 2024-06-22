1d49d0fa7b2e1d59fb99260347c49706a4c12f25
Aidan Bui <aidan@techreative.com.au>
2024-06-22 10:58:48 +1000

    Added Meteor/React stack instead of just React, finished backend

--- server/app/views.py
+++ server/app/views.py
@@ -8,7 +8,7 @@ from .serializers import IssueSerializer
 class IssueApiView(APIView):
 
     def get(self, request, *args, **kwargs):
-        issues = Issue.objects.filter(user = request.user.id)
+        issues = Issue.objects.filter(issue_id = request.data.get('issue_id'))
         serializer = IssueSerializer(issues, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -19,9 +19,49 @@ class IssueApiView(APIView):
             'title': request.data.get('title'), 
             'description': request.data.get('description'), 
         }
+        
+        exists = Issue.objects.filter(issue_id=request.data.get('issue_id'))
+        if exists.count() > 0:
+            return Response(
+                {"res": "Object with issue id already exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         serializer = IssueSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, *args, **kwargs):
+
+        issue_instance = Issue.objects.get(issue_id=request.data.get('issue_id'))
+        if not issue_instance:
+            return Response(
+                {"res": "Object with issue id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'issue_id': request.data.get('issue_id'), 
+            'title': request.data.get('title'), 
+            'description': request.data.get('description'), 
+        }
+        serializer = IssueSerializer(instance = issue_instance, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        issue_instance = Issue.objects.get(issue_id=request.data.get('issue_id'))
+        if not issue_instance:
+            return Response(
+                {"res": "Object with issue id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        issue_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
